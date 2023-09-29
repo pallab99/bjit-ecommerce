b6d87b16a4904c1c5937675adef4dfe28ca23c48
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-09-29 15:35:23 +0600

    done

--- src/components/showBookDetails/index.jsx
+++ src/components/showBookDetails/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 import useFetchData from "../../hooks/useApiFetch";
 import { useParams, useNavigate } from "react-router-dom";
@@ -11,13 +11,24 @@ const Index = () => {
   const { bookId } = useParams();
   const URL = `http://localhost:8000/api/books/details/${bookId}`;
   const { data, isLoading } = useFetchData(URL);
+
   const navigate = useNavigate();
   const handleUpdateBook = () => {
     navigate(`/updateBook/${bookId}`);
   };
 
   const { handleDeleteBook } = useDeleteBook(bookId);
-
+  const [images] = useState([
+    {
+      url: "https://www.electronickits.com/wp-content/uploads/2015/02/products-EFDNEW-e1423809441405.jpg",
+    },
+    {
+      url: "https://target.scene7.com/is/image/Target/GUEST_ba3fbb9a-daf5-4c17-8e62-3ba620852fb6?wid=488&hei=488&fmt=pjpeg",
+    },
+    {
+      url: "https://qph.cf2.quoracdn.net/main-qimg-880a65f86b1d5d73a962ee2520459af7-lq",
+    },
+  ]);
   return (
     <>
       {isLoading ? (
@@ -27,10 +38,9 @@ const Index = () => {
           <Navbar></Navbar>
           <div className="book-details">
             <div className="book-details-img-div">
-              <img
-                src="https://qph.cf2.quoracdn.net/main-qimg-880a65f86b1d5d73a962ee2520459af7-lq"
-                alt=""
-              />
+              {images.map((ele, index) => {
+                return <img key={index} src={ele.url} alt="" />;
+              })}
             </div>
             <div className="details-description">
               <h2>Title : {data?.data?.result?.title}</h2>
@@ -42,7 +52,7 @@ const Index = () => {
             </div>
             <div className="btn-div-update-dlt">
               <Button
-                className={"add-to-cart-btn"}
+                className={"update-btn"}
                 text={"Update book"}
                 handleButtonClick={handleUpdateBook}
               ></Button>
--- src/hooks/useDeleteBook.js
+++ src/hooks/useDeleteBook.js
@@ -15,14 +15,11 @@ const useDeleteBook = (bookId) => {
       if (response.ok) {
         console.log("Book deleted successfully");
         navigate("/");
-        // Perform any additional actions after successful deletion
       } else {
         console.log("Error occurred while deleting book");
-        // Handle error case
       }
     } catch (error) {
       console.log("Error occurred while deleting book:", error);
-      // Handle error case
     }
   };
   return { handleDeleteBook };
