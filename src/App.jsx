056f21392e41d98890491b81700a6b679301f435
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-10-03 15:04:24 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
-import { CookiesProvider } from "react-cookie";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Cart from "./pages/cart";
@@ -12,22 +11,20 @@ import Registration from "./pages/registration";
 import CodeVerification from "./pages/codeVerification";
 function App() {
   return (
-    <CookiesProvider>
-      <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/login" Component={Login}></Route>
-        <Route path="/cart" Component={Cart}></Route>
-        <Route path="/createBook" Component={CreateBook}></Route>
-        <Route path="/updateBook/:bookId" Component={UpdateBook}></Route>
-        <Route
-          path="/showBookDetails/:bookId"
-          Component={ShowBookDetails}
-        ></Route>
-        <Route path="/signUp" Component={Registration}></Route>
-        <Route path="/verifyCode" Component={CodeVerification}></Route>
-        <Route path="*" Component={NotFound}></Route>
-      </Routes>
-    </CookiesProvider>
+    <Routes>
+      <Route path="/" Component={Home} />
+      <Route path="/login" Component={Login}></Route>
+      <Route path="/cart" Component={Cart}></Route>
+      <Route path="/createBook" Component={CreateBook}></Route>
+      <Route path="/updateBook/:bookId" Component={UpdateBook}></Route>
+      <Route
+        path="/showBookDetails/:bookId"
+        Component={ShowBookDetails}
+      ></Route>
+      <Route path="/signUp" Component={Registration}></Route>
+      <Route path="/verifyCode" Component={CodeVerification}></Route>
+      <Route path="*" Component={NotFound}></Route>
+    </Routes>
   );
 }
 
